9b31a291a4e27161072ad258e1e87c5e2b55adb2
Luc-h0 <hodinhduyluc@gmail.com>
2025-03-30 22:54:54 +0700

    fix configuration

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ import dashboard  # Import the dashboard module
 import cv  # Import the CV module
 import cover_letter  # Import the cover letter module
 
+# Set wide mode
+st.set_page_config(layout="wide")
+
 @st.cache_data
 def load_data():
     return pd.read_csv("vgsales.csv")
 
 # Load dataset
 df = load_data()
-
-# Set wide mode
-st.set_page_config(layout="wide")
 
 # Streamlit Sidebar
 st.sidebar.title("📊 Video Game Sales Dashboard")
